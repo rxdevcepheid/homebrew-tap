2c2df2e5de766ac31c6f7838a1b2c5aded0db287
pmanala <m.pavan.se@gmail.com>
2018-07-10 09:19:41 +0200

    Update elasticsearch-1.2.2.rb
    Updated the formula

--- elasticsearch-1.2.2.rb
+++ elasticsearch-1.2.2.rb
@@ -78,7 +78,7 @@ class Elasticsearch122 < Formula
     (var/"lib/elasticsearch/plugins").mkpath
   end
 
-  def caveats; <<-EOS.undent
+  def caveats; <<-EOS
     Data:    #{var}/elasticsearch/#{cluster_name}/
     Logs:    #{var}/log/elasticsearch/#{cluster_name}.log
     Plugins: #{var}/lib/elasticsearch/plugins/
@@ -87,7 +87,7 @@ class Elasticsearch122 < Formula
 
   plist_options :manual => "elasticsearch --config=#{HOMEBREW_PREFIX}/opt/elasticsearch/config/elasticsearch.yml"
 
-  def plist; <<-EOS.undent
+  def plist; <<-EOS
       <?xml version="1.0" encoding="UTF-8"?>
       <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
       <plist version="1.0">
