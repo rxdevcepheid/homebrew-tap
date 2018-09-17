cedb068250333cfb3bf4d4be8c024947662affa9
Lucas Persson <lucas.persson@cepheid.se>
2018-09-17 11:32:53 +0200

    update flyway

--- flyway@5.1.4.rb
+++ flyway@5.1.4.rb
@@ -2,7 +2,7 @@ class FlywayAT514 < Formula
   desc "Database version control to control migrations"
   homepage "https://flywaydb.org/"
   url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/5.1.4/flyway-commandline-5.1.4.tar.gz"
-  sha1 "812660587feffa4d2f8869ad4530e52a755037cf"
+  sha256 "f8388184f8a37b950d892d5ddc8919259ff3bb64d49bfb90ea6697328b085fe5"
 
   bottle :unneeded
 
