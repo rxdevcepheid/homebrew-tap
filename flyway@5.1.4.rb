b31dc781d16d261f85bd62d5b0d96f7a340fe8ce
Lucas Persson <lucas.persson@cepheid.se>
2018-09-17 11:28:19 +0200

    update flyway

---
+++ flyway@5.1.4.rb
@@ -0,0 +1,20 @@
+class FlywayAT514 < Formula
+  desc "Database version control to control migrations"
+  homepage "https://flywaydb.org/"
+  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/5.1.4/flyway-commandline-5.1.4.tar.gz"
+  sha256 "812660587feffa4d2f8869ad4530e52a755037cf"
+
+  bottle :unneeded
+
+  depends_on :java
+
+  def install
+    rm Dir["*.cmd"]
+    libexec.install Dir["*"]
+    bin.install_symlink Dir["#{libexec}/flyway"]
+  end
+
+  test do
+    system "#{bin}/flyway", "-url=jdbc:h2:mem:flywaydb", "validate"
+  end
+end
