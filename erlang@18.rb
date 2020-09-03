864429e4faab56d0b15cf2b78b3e818c7d0eac7f
Lucas Persson <Lucas3oo@users.noreply.github.com>
2020-09-03 21:51:38 +0200

    try to add a bottle

--- erlang@18.rb
+++ erlang@18.rb
@@ -4,6 +4,12 @@ class ErlangAT18 < Formula
   url "https://github.com/erlang/otp/archive/OTP-18.3.4.tar.gz"
   sha256 "7ab381d64a2943a35782f173792e4c2678ae9fd9bffba1f2814ffe701070c1bc"
 
+  bottle do
+    cellar :any
+    sha256 "77dc8acc693bda09f9e06fd36196d4aa6e3320585c1012b879ab4cd79a7f6322" => :catalina
+  end
+
+
   keg_only :versioned_formula
 
   option "without-hipe", "Disable building hipe; fails on various macOS systems"
