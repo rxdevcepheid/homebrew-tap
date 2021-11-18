730fef999271afd411ddd986f0b0e48b8b90a588
pmanala <m.pavan.se@gmail.com>
2021-11-18 14:34:54 +0100

    Update flyway@7.8.2.rb

--- flyway@7.8.2.rb
+++ flyway@7.8.2.rb
@@ -15,7 +15,7 @@ class FlywayAT782 < Formula
   def install
     rm Dir["*.cmd"]
     libexec.install Dir["*"]
-    (bin/"flyway").write_env_script libexec/"flyway"
+    (bin/"flyway").write_env_script libexec/"flyway", JAVA_HOME: Formula["openjdk"].opt_prefix 
   end
 
   test do
